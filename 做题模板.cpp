d19624bae7bdcfb349b4fd9355353107063bf675
m0tky <43568769+m0tky@users.noreply.github.com>
2019-04-07 21:06:58 +0800

    first
    模板

---
+++ 做题模板.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<algorithm> 
+#include<map>
+#include<vector>
+#include<queue>  
+using namespace std;   
+    
+
+int main(){    
+#ifdef ONLINE_JUDGE    
+#else    
+    freopen("1.txt", "r", stdin);    
+#endif    
+	
+    /* your code */
+  
+    return 0;    
+}
